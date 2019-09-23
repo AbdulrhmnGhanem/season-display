ff5192c36f5c1d45ba4d1f1566056e7236dc741e
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-23 22:07:47 +0200

    Rfactor to use fuctional component instead of CB components

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
 
-class App extends React.Component {
-    state = {lat: null, errorMessage: ''};
+const App = () => {
+    const [lat, setLat] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    componentDidMount() {
+    useEffect(() => {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({lat: position.coords.latitude}),
-            (err) => this.setState({errorMessage: err.message})
-        );
-    }
-
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
-        }
-
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={ this.state.lat }/>
-        }
-
-        return <Spinner message="Please, accept the location request"/>
-
-    }
-
-    render() {
-        return(
-            <div className='border red'>
-                {this.renderContent()}
-            </div>
+            (position) => setLat({lat: position.coords.latitude}),
+            (err) => setErrorMessage({errorMessage: err.message})
         )
+    }, []);
+
+    let content;
+
+    if (errorMessage) {
+        content = <div>Error: {errorMessage}</div>
+    } else if (lat) {
+        content = <SeasonDisplay lat={ lat }/>
+    } else {
+        content = <Spinner message="Please, accept the location request"/>
     }
-}
+
+    return (
+        <div className="border red">
+            { content }
+        </div>
+    )
+
+};
 
 ReactDOM.render(
     <App/>,
